f9ce0226a6c4dc85e77abea602a1e848b4f51d9e
Asus Vivobook <dmitriyskrill@gmail.com>
2025-04-07 22:28:05 +0300

    debug

--- src/yandexTracker/service/lib/calculateHoursFromDuration.js
+++ src/yandexTracker/service/lib/calculateHoursFromDuration.js
@@ -9,7 +9,7 @@ function calculateHoursFromDuration (data) {
   const days = duration?.$d?.days || 0
   const hours = duration?.$d?.hours || 0
   const minutes = duration?.$d?.minutes || 0
-  return Math.round(days * 8) + hours + Math.round(minutes / 60)
+  return days * 8 + hours + minutes / 60
 }
 
 export {
